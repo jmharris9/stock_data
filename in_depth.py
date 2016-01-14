a390e97467c5bebaea9b7e306fd75d6672a0dc56
James Harris <jmharris9@gmail.com>
2016-01-14 01:04:07 -0500

    1/14/16 commit

--- in_depth.py
+++ in_depth.py
@@ -13,14 +13,12 @@ import sqlite3 as lite
 #To Run: Call program python in_depth.py SPREADSHEET_NAME INDUSTRY
 def get_industry_symbols():
     industry = sys.argv[2]
-    response = requests.get("http://www.investorguide.com/industry/"+industry)
+    response = requests.get("https://biz.yahoo.com/"+industry)
     html_body = html.fromstring(response.text)
-    links =  html_body.xpath('//div[@class="column one-half"]//a/@href')
+    links =  html_body.xpath('//td[@bgcolor="ffffee"]//a[position()=2]')
     tickers = []
     for link in links:
-        if "ticker" in link:
-            ticker= re.search('=.*', link).group(0)[1:]
-            tickers.append(ticker)
+        tickers.append(link.text)
     return tickers
 
 def print_multi_year(row, row_to_analyze, worksheet, cell_format):
@@ -35,6 +33,7 @@ def print_multi_year(row, row_to_analyze, worksheet, cell_format):
     worksheet.write_formula('I'+str(row),'{=IF(AND(E'+r+'>0,I'+r+'>0),(I'+r+'/E'+r+')^(1/5)-1,0)}',cell_format)
     worksheet.write_formula('J'+str(row),'{=IF(AND(F'+r+'>0,J'+r+'>0),(J'+r+'/F'+r+')^(1/5)-1,0)}',cell_format)
     worksheet.write_formula('K'+str(row),'{=IF(AND(G'+r+'>0,K'+r+'>0),(K'+r+'/G'+r+')^(1/5)-1,0)}',cell_format)
+    worksheet.write_formula('L'+str(row),'{=IF(AND(H'+r+'>0,L'+r+'>0),(L'+r+'/H'+r+')^(1/5)-1,0)}',cell_format)
 
 def get_db_data(cur, key, stock_id):
     cur.execute('SELECT * FROM '+key+' WHERE stock_id='+str(stock_id)+'')
@@ -42,14 +41,17 @@ def get_db_data(cur, key, stock_id):
         data = np.array(cur.fetchall()[0])
         data_corrected = data/100
     else:
-        data =  np.array(cur.fetchall()[0])
+        try:
+            data =  np.array(cur.fetchall()[0])
+        except IndexError:
+            data = np.array(0,0,0,0,0,0,0,0,0,0,0)
         data_corrected=data/1000000
     return data_corrected
 
 def graham_analysis():
     stock_symbols = get_industry_symbols()## Change between sector vs individual modes
     title =sys.argv[1]
-    #stock_symbols = #["VVTV", "SPF", "TAIT", "CRV", "BZH", "MSN", "TUES", "HDNG"]
+    #stock_symbols =["AAPL", "AGX", "BBSI", "BBY", "BPT", "BSQR", "BKE", "CALM", "CPLA", "CSCO", "CLCT", "DLX", "ENTA", "FLR", "GME", "GILD", "GPRO", "GHC", "ONE", "HPQ", "IQNT", "IDCC", "IILG", "ESI", "KING", "LCI", "MTEX", "MCFT", "MSB", "KORS", "MSGN", "NSR", "NLNK", "OUTR", "PDLI", "PETS", "PPC", "PBI", "PII", "QCOM", "RPXC", "TDC", "TIVO", "UNTD", "UTHR", "VDSI", "VEC", "VIAB", "VNCE", "WLDN"]
       #"GM", "USG", "MCO", "DVA", "DTV", "XOM", "PG", "WMT", "IBM", "KO"]#"CNTF", "GRVY", "XIN"]#"GILD","MRK","ABBV","VRTX","GSK","BMY","PFE","NVS"]
     graham_dict = {}
     workbook = xlsxwriter.Workbook(title+'.xlsx')
@@ -66,13 +68,16 @@ def graham_analysis():
     col = list(string.ascii_uppercase)
     number_of_tickers = len(stock_symbols)
     j=0
-    while len(col)<number_of_tickers:
+    if number_of_tickers >650:
+        letter_length = 650
+    else:
+        letter_length = number_of_tickers
+    while len(col)<letter_length:
         i=0
         while i<26:
             col.append(list(string.ascii_uppercase)[j]+list(string.ascii_uppercase)[i])
             i=i+1
         j=j+1
-
     ### SET WORKSHEET FORMATS
     worksheet.set_column(0, 0, 50)
     worksheet.conditional_format('B19:'+col[-1]+'23', {'type': 'cell', 'criteria':'<','value':0,'format':warning})
@@ -158,6 +163,7 @@ def graham_analysis():
         for stock in stock_symbols:
             success = ws.download_data(stock)
             if success == False:
+                print stock
                 continue
             worksheet.write(col[i]+'1', stock, bold)
 
@@ -166,7 +172,7 @@ def graham_analysis():
             cur.execute('SELECT Id FROM Stocks WHERE symbol="'+stock+'"')
             index = cur.fetchall()[0][0]
             long_term_debt_bs = get_db_data(cur, "long_term_debt_bs", index)
-            print long_term_debt_bs[10]
+            #print long_term_debt_bs[10]
             worksheet.write(col[i]+'4', get_db_data(cur, "long_term_debt_bs", index)[10], num_format)
             worksheet.write(col[i]+'5', get_db_data(cur, "preferred_stock_bs", index)[10], num_format)
             worksheet.write(col[i]+'7', get_db_data(cur, "weighted_average_shares_outstanding_diluted_is", index)[11], num_format)
@@ -249,7 +255,7 @@ def graham_analysis():
             worksheet.write("A"+str(start+8), "Owners Earnings per share growth")
             worksheet.write("A"+str(start+9), "Croic")
             k=1
-            print num_years
+            #print num_years
             while k<num_years:
                 worksheet.write(col[k]+str(start+1), get_db_data(cur, "ebitda_is", index)[k], num_format)
                 worksheet.write(col[k]+str(start+2), get_db_data(cur, "depreciation_and_amortization_cf", index)[k], num_format)
@@ -261,17 +267,17 @@ def graham_analysis():
                 k=k+1
             print_multi_year(start+7,start+5,worksheet, percent_format)
             print_multi_year(start+8,start+6,worksheet, percent_format)
-            worksheet.write_formula(col[k]+str(start+7), '{=MEDIAN(B'+str(start+7)+':'+col[k-2]+str(start+7)+')}', percent_format)
-            worksheet.write_formula(col[k]+str(start+8), '{=MEDIAN(B'+str(start+8)+':'+col[k-2]+str(start+8)+')}', percent_format)
-            worksheet.write_formula(col[k]+str(start+9), '{=MEDIAN(B'+str(start+9)+':'+col[k-2]+str(start+9)+')}', percent_format)
+            worksheet.write_formula(col[k]+str(start+7), '{=MEDIAN(B'+str(start+7)+':'+col[k-1]+str(start+7)+')}', percent_format)
+            worksheet.write_formula(col[k]+str(start+8), '{=MEDIAN(B'+str(start+8)+':'+col[k-1]+str(start+8)+')}', percent_format)
+            worksheet.write_formula(col[k]+str(start+9), '{=MEDIAN(B'+str(start+9)+':'+col[k-1]+str(start+9)+')}', percent_format)
             #worksheet.write(col[num_years]+str(start+2), -data_dict["depreciation (unrecognized)"][num_years-1])
 
             #########NEED TO FIGURE OUT WHAT TO DO WITH LESS THAN 10 yrs of data when computing growth rates
 
             ##### Growth ####
-            worksheet.write_formula(col[i]+'69', '{=MEDIAN(B'+str(start+7)+':K'+str(start+7)+')}', percent_format)
-            worksheet.write_formula(col[i]+'70', '{=MEDIAN(B'+str(start+8)+':K'+str(start+8)+')}', percent_format)
-            worksheet.write_formula(col[i]+'71', '{=MEDIAN(B'+str(start+9)+':K'+str(start+9)+')}', percent_format)
+            worksheet.write_formula(col[i]+'69', '{=MEDIAN(B'+str(start+7)+':L'+str(start+7)+')}', percent_format)
+            worksheet.write_formula(col[i]+'70', '{=MEDIAN(B'+str(start+8)+':L'+str(start+8)+')}', percent_format)
+            worksheet.write_formula(col[i]+'71', '{=MEDIAN(B'+str(start+9)+':L'+str(start+9)+')}', percent_format)
             i=i+1
             start = start+11
     except lite.Error, e:
@@ -283,6 +289,7 @@ def graham_analysis():
     workbook.close()
 
 def run():
+  #get_industry_symbols()
   graham_analysis()
 
 
